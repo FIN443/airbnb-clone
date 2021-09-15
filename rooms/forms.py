174784d9bd2b71463bd6ed3fd6398438debebe3f
FIN443 <jjhun1228@gmail.com>
2021-09-15 22:30:12 +0900

    #13.10 search view

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,8 +1,38 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import widgets
+from django.http import request
+from django_countries.fields import CountryField
+from pkg_resources import require
 from . import models
 
 
-class CreatePhotoForm(ModelForm):
+class SearchForm(forms.Form):
+
+    city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
+    )
+    price = forms.IntegerField(required=False)
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+    )
+
+
+class CreatePhotoForm(forms.ModelForm):
     class Meta:
         model = models.Photo
         fields = (
